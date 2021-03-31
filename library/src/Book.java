6c20e41cd062bd20b6cc070724db142833071a08
Łukasz Bińkowski <luk.binkowski@gmail.com>
2021-04-01 00:44:11 +0200

    Second commit test

---
+++ library/src/Book.java
@@ -0,0 +1,8 @@
+public class Book {
+    String title;
+    String author;
+    int releaseDate;
+    int pages;
+    String Publisher;
+    String isbn;
+}
--- library/src/Library.java
+++ library/src/Library.java
@@ -3,5 +3,7 @@ public class Library {
         final String appName = "Biblioteka v0.1";
 
         System.out.println(appName);
+
+        Book book1 = new Book();
     }
 }
