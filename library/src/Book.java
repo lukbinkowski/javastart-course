d5ee0037f7211a7cc0ab1114ef0ad854237aa82c
Łukasz Bińkowski <luk.binkowski@gmail.com>
2021-04-07 00:49:52 +0200

    class Book update

--- library/src/Book.java
+++ library/src/Book.java
@@ -5,4 +5,19 @@ public class Book {
     int pages;
     String Publisher;
     String isbn;
+
+    Book(String bookTitle, String bookAuthor, int bookReleaseD, int bookPages, String bookPublisher, String bookIsbn) {
+        title = bookTitle;
+        author = bookAuthor;
+        releaseDate = bookReleaseD;
+        pages = bookPages;
+        Publisher = bookPublisher;
+        isbn = bookIsbn;
+    }
+
+
+    void printInfo() {
+        String info = title + "; " + author + "; " + releaseDate + "; " + pages + "; " + Publisher + "; " + isbn;
+        System.out.println(info);
+    }
 }
