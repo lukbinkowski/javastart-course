eecb1a19c4469193622d6e812d8e1f4fa8700801
Łukasz Bińkowski <luk.binkowski@gmail.com>
2021-04-09 00:33:26 +0200

    class adaptation to JavaBeans

--- library/src/pl/javastart/library/app/Library.java
+++ library/src/pl/javastart/library/app/Library.java
@@ -1,6 +1,10 @@
+package pl.javastart.library.app;
+
+import pl.javastart.library.model.Book;
+
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.4s";
+        final String appName = "Biblioteka v0.5";
 
         System.out.println(appName);
 
