ffa033546e843d38a585418ce36ad398ed5e7446
lukbinkowski <luk.binkowski@gmail.com>
2023-03-15 00:57:45 +0100

    Adding Publication and Magazne class

--- library/src/pl/javastart/library/model/Library.java
+++ library/src/pl/javastart/library/model/Library.java
@@ -3,8 +3,11 @@ package pl.javastart.library.model;
 public class Library {
 
     private static final int MAX_BOOKS = 1000;
+    private static final int MAX_MAGAZINES = 1000;
     private Book[] books = new Book[MAX_BOOKS];
+    private Magazine[] magazines = new Magazine[MAX_MAGAZINES];
     private int booksNumber = 0;
+    private int magazinesNumber = 0;
 
     public void addBook(Book book) {
         if (booksNumber < MAX_BOOKS) {
@@ -15,7 +18,16 @@ public class Library {
         }
     }
 
-    public void printbooks() {
+    public void addMagazine(Magazine magazine) {
+        if (magazinesNumber < MAX_MAGAZINES) {
+            magazines[magazinesNumber] = magazine;
+            magazinesNumber++;
+        } else {
+            System.out.println("Maksymalna liczba czasopizm została osiągnięta");
+        }
+    }
+
+    public void printBooks() {
         if (booksNumber == 0) {
             System.out.println("Brak książek w bibliotece");
         }
@@ -23,4 +35,14 @@ public class Library {
             books[i].printInfo();
         }
     }
+
+    public void printMagazines() {
+        if (magazinesNumber == 0) {
+            System.out.println("Brak czasopizm w bibliotece");
+        }
+        for (int i = 0; i < magazinesNumber; i++) {
+            magazines[i].printInfo();
+        }
+    }
+
 }
